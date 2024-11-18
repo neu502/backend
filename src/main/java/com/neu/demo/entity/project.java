16d1d13508ca582f4be8b16bfe61c1f115c66205
zhangzixuan <2499319124@qq.com>
2024-11-18 10:36:47 +0800

    项目后端

---
+++ src/main/java/com/neu/demo/entity/project.java
@@ -0,0 +1,70 @@
+package com.neu.demo.entity;
+
+public class project {
+    int projectId;
+    String projectName;
+    String projectInfo;
+    String ddl;
+    String company;
+    String status;
+    int step;
+
+    public project() {
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectInfo() {
+        return projectInfo;
+    }
+
+    public void setProjectInfo(String projectInfo) {
+        this.projectInfo = projectInfo;
+    }
+
+    public String getDdl() {
+        return ddl;
+    }
+
+    public void setDdl(String ddl) {
+        this.ddl = ddl;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+}
---
+++ src/main/java/com/neu/demo/util/Result.java
@@ -0,0 +1,7 @@
+package com.neu.demo.util;
+
+//import lombok.Data;
+//创建接口统一返回类，暂时没有什么用
+//@Data
+public class Result {
+}
